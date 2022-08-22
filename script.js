eaae61740f49bd01b4a08ff37e4b7af319211a77
zhakevich <zhakevichv@ya.ru>
2022-08-22 15:49:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+let items = document.querySelectorAll('.carusel .item');
+let currentItem = 0;
+let isEnabled = true;
+
+function chengecurrentItem(n) {
+  currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+  isEnabled = false;
+  items[currentItem].classList.add(direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('active', direction);
+  });
+}
+
+function showItem(direction) {
+  items[currentItem].classList.add('next', direction);
+  items[currentItem].addEventListener('animationend', function () {
+    this.classList.remove('next', direction);
+    this.classList.add('active');
+    isEnabled = true;
+  });
+}
+
+function previousItem(n) {
+  hideItem('to-right');
+  chengecurrentItem(n - 1);
+  showItem('from-left');
+}
+
+function nextItem(n) {
+  hideItem('to-left');
+  chengecurrentItem(n + 1);
+  showItem('from-right');
+}
+
+document.querySelector('.control.left').addEventListener('click', function () {
+  if (isEnabled) {
+    previousItem(currentItem);
+  }
+});
+
+document.querySelector('.control.right').addEventListener('click', function () {
+  if (isEnabled) {
+    nextItem(currentItem);
+  }
+});
+
+// Сваипы мышью
+
+const swipedetect = (el) => {
+  let surface = el;
+  let startX = 0;
+  let startY = 0;
+  let distX = 0;
+  let distY = 0;
+
+  let startTime = 0;
+  let elepsedTime = 0;
+
+  let threshold = 150;
+  let restraint = 130;
+  let allowedTime = 300;
+
+  surface.addEventListener('mousedown', function (e) {
+    startX = e.pageX;
+    startY = e.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  });
+
+  surface.addEventListener('mouseup', function (e) {
+    distX = e.pageX - startX;
+    distY = e.pageY - startY;
+    elepsedTime = new Date().getTime() - startTime;
+
+    if (elepsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousItem(currentItem);
+          }
+        } else {
+          if (isEnabled) {
+            nextItem(currentItem);
+          }
+        }
+      }
+    }
+
+    e.preventDefault();
+  });
+
+  surface.addEventListener('touchstart', function (e) {
+    if (
+      e.target.classList.contains('arrow') ||
+      e.target.classList.contains('control')
+    ) {
+      if (e.target.classList.contains('left')) {
+        if (isEnabled) {
+          previousItem(currentItem);
+        }
+      } else if (e.target.classList.contains('right')) {
+        if (isEnabled) {
+          nextItem(currentItem);
+        }
+      }
+    }
+
+    let touchObj = e.changedTouches[0];
+    startX = touchObj.pageX;
+    startY = touchObj.pageY;
+    startTime = new Date().getTime();
+    e.preventDefault();
+  });
+
+  surface.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+  });
+
+  surface.addEventListener('touchend', function (e) {
+    let touchObj = e.changedTouches[0];
+    distX = touchObj.pageX - startX;
+    distY = touchObj.pageY - startY;
+    elepsedTime = new Date().getTime() - startTime;
+
+    if (elepsedTime <= allowedTime) {
+      if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+        if (distX > 0) {
+          if (isEnabled) {
+            previousItem(currentItem);
+          }
+        } else {
+          if (isEnabled) {
+            nextItem(currentItem);
+          }
+        }
+      }
+    }
+
+    e.preventDefault();
+  });
+};
+
+let el = document.querySelector('.carusel');
+swipedetect(el);
+
+// Tiny slider
+
+// http://ganlanyuan.github.io/tiny-slider/demo/
